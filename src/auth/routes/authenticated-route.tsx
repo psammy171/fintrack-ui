1cf89efd3e59cee370fd7ffb27d4b2d94e434f5c
Sammy P <psammy@Sammys-iMac.bwrouter>
2026-04-14 19:52:52 +0530

    fix: fix authentication architecture

--- src/auth/routes/authenticated-route.tsx
+++ src/auth/routes/authenticated-route.tsx
@@ -1,7 +1,4 @@
 import { useAuth } from "../hooks/use-auth";
-import { useEffect } from "react";
-import type { UserContext } from "../../types/user-context";
-import apiClient from "../../lib/axios";
 import { useLocation } from "react-router-dom";
 
 interface Props {
@@ -12,66 +9,18 @@ const AuthenticatedRoute = ({ children }: Props) => {
 	const location = useLocation();
 	const pathName = location.pathname;
 	const authRoutes = ["/expenses", "/dashboard", "/tags"];
-	const {
-		isAuthenticating,
-		authenticated,
-		setIsAuthenticating,
-		setUserContext,
-		setAuthenticated,
-		userContext,
-	} = useAuth();
-
-	useEffect(() => {
-		const checkAuth = async () => {
-			if (userContext && authenticated) {
-				return;
-			}
-			setAuthenticated(false);
-			setIsAuthenticating(true);
-
-			try {
-				const response = await apiClient.get("/profile");
-				const userContext: UserContext = response.data;
-				setUserContext(userContext);
-				setAuthenticated(true);
-			} catch (err) {
-				console.error(err);
-				setAuthenticated(false);
-			} finally {
-				setIsAuthenticating(false);
-			}
-		};
-		checkAuth();
-	}, [
-		authenticated,
-		setAuthenticated,
-		setIsAuthenticating,
-		setUserContext,
-		userContext,
-	]);
+	const { isAuthenticating, authenticated } = useAuth();
 
 	const isAuthRoute = () => authRoutes.includes(pathName);
 
-	const getElement = () => {
-		if (isAuthenticating) {
-			return (
-				<div className="h-screen w-full flex items-center justify-center">
-					<div>Loading...</div>
-				</div>
-			);
-		}
+	if (!isAuthenticating && !authenticated && isAuthRoute()) {
+		window.location.href =
+			import.meta.env.VITE_API_LOGIN_URL +
+			"&state=" +
+			window.location.pathname;
+	}
 
-		if (!authenticated && isAuthRoute()) {
-			window.location.href =
-				import.meta.env.VITE_API_LOGIN_URL +
-				"&state=" +
-				window.location.pathname;
-		}
-
-		return children;
-	};
-
-	return getElement();
+	return children;
 };
 
 export default AuthenticatedRoute;

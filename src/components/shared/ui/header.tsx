5e94c6a24e2c986f94168468e0333c6ab909f787
Sammy P <psammy@Sammys-iMac.bwrouter>
2026-06-17 19:49:18 +0530

    add bottom padding

--- src/components/shared/ui/header.tsx
+++ src/components/shared/ui/header.tsx
@@ -67,11 +67,6 @@ const Header = () => {
 					</Link>
 					<p className="text-xl font-bold">{getTitle()}</p>
 					<span className="flex-grow"></span>
-					{/* <Link to="/dashboard">Dashboard</Link>
-					<Link to="/expenses">Expenses</Link>
-					<Link to={'/settings'}>
-						<SettingsIcon className="w-5 h-5 hover:rotate-180 duration-300 cursor-pointer" />
-					</Link> */}
 					<span
 						className="border-2 w-7 h-7 rounded-full flex items-center justify-center cursor-pointer"
 						onClick={toggleProfile}
@@ -79,9 +74,11 @@ const Header = () => {
 						<p>{userContext.firstName.charAt(0)}</p>
 					</span>
 					<span
-						className={`absolute w-72 border border-gray-300 bg-gray-200 right-[6px] ${
-							open ? "-bottom-[140px]" : "bottom-0"
-						} transition-all duration-300 -z-30 p-4 rounded-lg text-gray-800 shadow-lg`}
+						className={`absolute w-72 border border-gray-300 bg-gray-200 right-[6px] top-14 ${
+							open
+								? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
+								: "opacity-0 scale-95 -translate-y-2 pointer-events-none"
+						} transition-all duration-300 p-4 rounded-lg text-gray-800 shadow-lg`}
 						onClick={(e) => e.stopPropagation()}
 					>
 						<span className="flex items-center gap-x-3">
